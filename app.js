7769411409d6987f627737c84760cc7cfdb78d80
Krohit9252 <r.rohit9252@gmail.com>
2024-03-12 12:38:23 +0530

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const dotenv=require('dotenv')
+const cors = require("cors");
+const morgan=require('morgan')
+const connectDB=require('./config/database')
+// const authRoutes=require('./routes/authRoutes')
+//dot config
+dotenv.config();
+
+//mongodb connection
+connectDB()
+
+//rest object
+const app = express()
+
+// Middleware
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+//Routes
+app.get('/',(req,res)=>{
+    res.send("Server is running")
+})
+
+app.use('/api/v1/admin',require('./routes/adminRoutes'))
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+
+
+app.listen(process.env.PORT||8080,console.log(`server is running on port ${process.env.PORT}`))
